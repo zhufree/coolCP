6f520bd271368dbe394e5bb0c10b406da3419ea9
zhufree <zhufree2013@gmail.com>
2016-05-19 23:03:41 +0800

    mongoose update

--- models/mongodb.js
+++ models/mongodb.js
@@ -1,25 +1,25 @@
 var mongoose = require('mongoose');
-var create
 mongoose.connect('mongodb://localhost/test');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
-    console.log('yay!');
-    var UserSchema = mongoose.Schema({
-        username : String,
-        nickname : String,
-        password : String,
-        register_date : {
-            type: Date,
-            default: Date.now
-        },
-    });
-    //将Schema发布为Model
-    var UserModel = mongodb.mongoose.model("User", UserSchema);
-    var free = new UserModel({username: 'free'});
-    console.log(free);
-    var UserDAO = function(){};
-    module.exports = new UserDAO();
+    
+
+    // var UserDAO = function(){};
+    // module.exports = new UserDAO();
 });
+var UserSchema = mongoose.Schema({
+    username : String,
+    nickname : String,
+    password : String,
+    register_date : {
+        type: Date,
+        default: Date.now
+    },
+});
+//将Schema发布为Model
+var UserModel = mongoose.model("User", UserSchema);
+var free = new UserModel({username: 'free'});
+exports.User = UserModel;
 exports.mongoose = mongoose;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var models = require('../models/mongodb');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
 var router = express.Router();
-
+var User = models.User;
+console.log(models);
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -13,7 +17,10 @@ router.get('/logout', function(req, res, next){
   res.send('logout');
 });
 router.get('/register', function(req, res, next){
-  res.send('register');
+	var free = new User({username: 'free'}); 
+	free.save();
+	console.log(free);
+  res.send(free.username);
 });
 
 module.exports = router;
